a7a320de491d5b60a334441a36d762b89e24e480
RacheMorris16k <rachemorris16@yahoo.com>
2025-05-02 23:55:31 -0400

    api

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "14883d1d2dccc4ad171191e5e529d0a2"
+
 
 # Geoapify API Key
-geoapify_key = 8d5bb64dca46481c993e08089664ac7b
+geoapify_key = "8d5bb64dca46481c993e08089664ac7b"
